424027839286ce9f42fbb1d94240006c62e5c196
rajpatel-softdev <125112439+rajpatel-softdev@users.noreply.github.com>
2023-07-17 10:11:47 +0530

    Add files via upload
    changes made in Q 1 to 5

---
+++ src/main/kotlin/que_2.kt
@@ -0,0 +1,21 @@
+fun main(){
+    val a=5
+    val b="abc"
+    val c=4.5049854534345
+    val d=true
+    val e=127
+    val f=3.7
+    val g='a'
+    val h=3489573457348957
+    val i=564
+    
+    println("Integer Value:$a")
+    println("String Value:$b")
+    println("Double Value:$c")
+    println("Boolean Value:$d")
+    println("Byte Value:$e")
+    println("Float Value:$f")
+    println("Character Value:$g")
+    println("Long Value:$h")
+    println("Short Value:$i")
+}
